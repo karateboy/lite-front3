7dddf5110f72ca0a3ae061e48a575e4d1b809736
karateboy <karateboy.tw@gmail.com>
2026-05-25 18:10:04 +0800

    init check in

---
+++ src/@layouts/components/HorizontalNav.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { HorizontalNavGroup, HorizontalNavLink } from '@layouts/components';
+import type { HorizontalNavItems, NavGroup, NavLink } from '@layouts/types';
+
+defineProps<{
+  navItems: HorizontalNavItems
+}>()
+
+const resolveNavItemComponent = (item: NavLink | NavGroup) => {
+  if ('children' in item)
+    return HorizontalNavGroup
+
+  return HorizontalNavLink
+}
+</script>
+
+<template>
+  <ul class="nav-items">
+    1234
+    <Component
+      :is="resolveNavItemComponent(item)"
+      v-for="(item, index) in navItems"
+      :key="index"
+      data-allow-mismatch
+      :item="item"
+    />
+  </ul>
+</template>
+
+<style lang="scss">
+.layout-wrapper.layout-nav-type-horizontal {
+  .nav-items {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+</style>
